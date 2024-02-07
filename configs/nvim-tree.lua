798ff9f604383e8b27b6e35c45522e216a3f4c19
Bill Finger <ur.narmak@gmail.com>
2024-02-07 06:32:10 +0300

    nvim-tree mappings

---
+++ configs/nvim-tree.lua
@@ -0,0 +1,31 @@
+local function edit_or_open()
+  local api = require "nvim-tree.api"
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file
+    api.node.open.edit()
+    -- Close the tree if file was opened
+    api.tree.close()
+  end
+end
+
+-- open as vsplit on current node
+local function vsplit_preview()
+  local api = require "nvim-tree.api"
+  local node = api.tree.get_node_under_cursor()
+
+  if node.nodes ~= nil then
+    -- expand or collapse folder
+    api.node.open.edit()
+  else
+    -- open file as vsplit
+    api.node.open.vertical()
+  end
+
+  -- Finally refocus on tree if it was lost
+  api.tree.focus()
+end
--- mappings.lua
+++ mappings.lua
@@ -9,11 +9,10 @@ M.general = {
         require("conform").format()
       end,
       "formatting",
-    }
-
+    },
   },
   v = {
-    [">"] = { ">gv", "indent"},
+    [">"] = { ">gv", "indent" },
   },
 }
 
@@ -27,24 +26,70 @@ M.dap = {
     ["<leader>dr"] = {
       "<cmd> DapContinue <CR>",
       "Start or continue the debugger",
-    }
-  }
+    },
+  },
 }
 M.lazygit = {
   n = {
     ["<leader>gl"] = {
       "<cmd> LazyGit <CR>",
-      "LazyGit"
+      "LazyGit",
     },
     ["<leader>gf"] = {
       "<cmd> LazyGitCurrentFile <CR>",
-      "LazyGit current file"
+      "LazyGit current file",
     },
     ["<leader>gc"] = {
       "<cmd> LazyGitFilter <CR>",
-      "LazyGit commits"
+      "LazyGit commits",
+    },
+  },
+}
+
+M.nvimtree = {
+  n = {
+    ["l"] = {
+      function()
+        local api = require "nvim-tree.api"
+        local function edit_or_open()
+          local node = api.tree.get_node_under_cursor()
+          if node.nodes ~= nil then
+            api.node.open.edit()
+          else
+            api.node.open.edit()
+            api.tree.close()
+          end
+        end
+        edit_or_open()
+      end,
+      "Edit Or Open",
+    },
+    ["L"] = {
+      function()
+        local api = require "nvim-tree.api"
+        local function vsplit_preview()
+          local node = api.tree.get_node_under_cursor()
+          if node.nodes ~= nil then
+            api.node.open.edit()
+          else
+            api.node.open.vertical()
+          end
+          api.tree.focus()
+        end
+        vsplit_preview()
+      end,
+    },
+    ["h"] = {
+      function()
+        require("nvim-tree.api").tree.close()
+      end
+    },
+    ["H"] = {
+      function()
+        require("nvim-tree.api").tree.collapse_all()
+      end
     }
-  }
+  },
 }
 
 -- more keybinds!
