1c913eebbc1c02f5d80c87e64e5f24e0b30a9d0b
Bill Finger <ur.narmak@gmail.com>
2024-02-08 14:34:20 +0300

    move dap config to other file

--- init.lua
+++ init.lua
@@ -1,24 +1,4 @@
 vim.opt.relativenumber = true
+vim.opt.cmdheight = 0
 
-vim.api.nvim_set_hl(0, "red", { fg = "#e06c75" })
-vim.api.nvim_set_hl(0, "green", { fg = "#9ece6a" })
-vim.api.nvim_set_hl(0, "yellow", { fg = "#FFFF00" })
-vim.api.nvim_set_hl(0, "orange", { fg = "#f09000" })
-
-vim.fn.sign_define(
-  "DapBreakpoint",
-  { text = "⬤", texthl = "red", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapBreakpointCondition",
-  { text = "⬤", texthl = "red", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapBreakpointRejected",
-  { text = "⬤", texthl = "orange", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define("DapStopped", { text = "•", texthl = "green", linehl = "DapBreakpoint", numhl = "DapBreakpoint" })
-vim.fn.sign_define(
-  "DapLogPoint",
-  { text = "⬤", texthl = "yellow", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
+require "custom.configs.dap-highlights"
